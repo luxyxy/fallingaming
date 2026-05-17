cd21ed0ebc7986c1507a95f70fdc90b2ef179a61
luxyxy <fukutu001@gmail.com>
2026-05-17 16:19:07 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,7 @@
-// Firebaseの設定
+// firebase-config.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCNgQe1bNjJbjiZOZiExkTMXOmaYRwkkIk",
   authDomain: "fallingaming-4fa8b.firebaseapp.com",
@@ -8,20 +11,5 @@ const firebaseConfig = {
   appId: "1:570079611483:web:8d43ba9562e8d9f8334d7e"
 };
 
-// Firebaseの初期化
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-// ランキングデータの取得（初期化）
-function loadRankings() {
-  const db = getFirestore(app);
-  const snapshot = query(collection(db, "ranking"));
-  const querySnapshot = getDocs(snapshot);
-  querySnapshot.then((docs) => {
-    console.log("ランキング取得完了:", docs);
-  });
-}
-
-// ファイアベースに接続するための関数
-export { app, auth, db };
+export const db = getFirestore(app);
